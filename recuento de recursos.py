b765c73a695ad28e7484fd1d53a27d45e9c44761
Mateo <tsuhikuriislife@gmail.com>
2026-03-03 12:57:20 -0500

    recuento de recursos v1

---
+++ recuento de recursos.py
@@ -0,0 +1,12 @@
+print(f'DIA {dias}...')
+print('Recursos iniciales del dia: ') # 1. Recuento del dia
+print(f'Servidores: {servidores}')
+print(f'Temperatura: {temperatura}')
+print(f'Clientes: {clientes}')
+
+
+print('Evento ocurrido: ', evento['nombre']) # Se notifica el evento
+print('Recursos afectados: ')
+print('servidores: ', evento['servidores'])
+print('temperatura: ', evento['temperatura'])
+print('clientes: ', evento['clientes'])
