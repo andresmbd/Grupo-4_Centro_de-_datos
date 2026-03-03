77ed997399befb054f0ff9f718af119cdf2ff0af
Jonathan <joanthan114311@gmail.com>
2026-03-03 12:32:10 -0500

    menu actualizado

--- menu.py
+++ menu.py
@@ -23,27 +23,7 @@ while (sw):
         print("invalid, please entar a correct option")
 
 
-if option == "1" or option == "option 1":
-    print("""wel, now you have to chosse a dificulted
-          option 1: easy
-          option 2: middle
-          option 3: dificult """)
-    
-df = True
 
-while (df):
-    Option = str(input("enter an option: "))
-    if Option == "1" or Option == "option 1":
-        df = False
-
-    elif Option == "2" or Option == "option 2":
-        df = False 
-
-    elif Option == "3" or Option == "option 3":
-        df = False
-
-    else:
-        print("invalid, please try again")
 
 
 
