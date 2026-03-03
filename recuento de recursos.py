27cdb41cc99eb45c8bd563dec727b7bc349a39ba
Mateo <tsuhikuriislife@gmail.com>
2026-03-03 13:00:34 -0500

    se añadio conteo de poblacion

--- recuento de recursos.py
+++ recuento de recursos.py
@@ -3,6 +3,7 @@ print('Recursos iniciales del dia: ') # 1. Recuento del dia
 print(f'Servidores: {servidores}')
 print(f'Temperatura: {temperatura}')
 print(f'Clientes: {clientes}')
+print(f'Poblacion: {clientes}')
 
 
 print('Evento ocurrido: ', evento['nombre']) # Se notifica el evento
