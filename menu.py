8bbcdc214f4a2ea6094d5b9d6ed4c5ecb47b5534
Jonathan <joanthan114311@gmail.com>
2026-03-03 12:23:28 -0500

    menu para el usuario con nivel de dificultad

--- menu.py
+++ menu.py
@@ -1,8 +1,52 @@
+print("data center control")
+
+
 print("""This is the menu of the game:
       
       you have two options:
       option 1: start
       option 2: exit""")
 
-option = input("enter an option: ")
+1
+
+sw = True
+
+while (sw):
+    option = str(input("enter an option: "))
+    if option == "1" or option == "option 1":
+        sw = False
+
+    elif option == "2" or option == "option 2":
+        sw = False
+
+    else:
+        print("invalid, please entar a correct option")
+
+
+if option == "1" or option == "option 1":
+    print("""wel, now you have to chosse a dificulted
+          option 1: easy
+          option 2: middle
+          option 3: dificult """)
+    
+df = True
+
+while (df):
+    Option = str(input("enter an option: "))
+    if Option == "1" or Option == "option 1":
+        df = False
+
+    elif Option == "2" or Option == "option 2":
+        df = False 
+
+    elif Option == "3" or Option == "option 3":
+        df = False
+
+    else:
+        print("invalid, please try again")
+
+
+
+    
+
 
