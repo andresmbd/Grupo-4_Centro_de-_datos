c777b452203eeafc5b2f29d7c8e6ff5df9e89220
Mateo <tsuhikuriislife@gmail.com>
2026-03-05 15:34:19 -0500

    reestructured motor.py to be able to use it in main.py

--- motor.py
+++ motor.py
@@ -1,9 +1,8 @@
-for dia in range(1, 11):  # del 1 al 10
-    print(f"Día {dia}: ¡Hola, hoy es un nuevo día!")
-    # Ciclo while para 10 días
-dia = 1  # empezamos en el día 1
-
-while dia <= 10:  # mientras dia sea menor o igual a 10
-    print(f"Día {dia}: ¡Hola, hoy es un nuevo día!")
-    dia += 1  # pasamos al siguiente día
+while gameplay_exit == 0:
+    days_played += 1
+    recuento_recursos(energy,  cooling_system, server_capacity, user_amount, days_played)
+    gameplay_exit = win_con(days_played, gameplay_exit)
+    if gameplay_exit > 0:
+        break
+    salto = input("Next day... (Press ENTER)")
     
